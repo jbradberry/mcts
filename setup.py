aab8693cbaed3f6ed0dd3197ca0b62d5c616deed
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-09-02 19:09:49 -0400

    Namespaced the entrypoint for the UCT player to jrb.mcts.uct.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jeff.bradberry@gmail.com',
     packages=['mcts'],
     entry_points={
-        'jrb_board.players': 'jrb.mcts = mcts.uct:MonteCarlo',
+        'jrb_board.players': 'jrb.mcts.uct = mcts.uct:MonteCarlo',
     },
     license='LICENSE',
     description="An implementation of UCT Monte Carlo Tree Search.",
